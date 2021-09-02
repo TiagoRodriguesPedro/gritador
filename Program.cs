cc4ef51d0a8a616505aee2077f82f490dc4dc303
Tiago Rodrigues <isatiago098@gmail.com>
2021-09-02 18:04:51 -0300

    resoliçao exercicio gritador

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string frasedigitada; 
+            Console.WriteLine("digite uma frase");
+            frasedigitada = Console.ReadLine();
+
+            Console.WriteLine("frasedigitada.toUpper()");
+        }
+    }
+}
